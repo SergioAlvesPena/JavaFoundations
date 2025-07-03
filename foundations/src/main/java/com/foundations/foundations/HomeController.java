9fd35c813e56c79e3d9ada2c658fd4504778a494
Sergio Pena <sergio.penaneto@gmail.com>
2025-07-03 18:05:41 -0300

    added a hello world

--- foundations/src/main/java/com/foundations/foundations/HomeController.java
+++ foundations/src/main/java/com/foundations/foundations/HomeController.java
@@ -1,11 +1,16 @@
 package com.foundations.foundations;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 public class HomeController {
 
-    public String Home(){
+    @RequestMapping("/hello", method=RequestMethod.GET)
+    public String Home(@RequestParam String param) {
         return "Hello World";
     }
     
